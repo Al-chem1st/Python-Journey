3762118673696708c2a852351ad31cbd345d04c4
Em0n <cmrakibul.official@gmail.com>
2026-09-04 21:31:37 +0600

    convert variables

---
+++ Day 1/type-casting.py
@@ -0,0 +1,12 @@
+#Typecasting is the process of converting a variable from one data type to another. 
+# like integer to float or string to inreger 
+
+name = "Alchemist"
+age = 22    
+gpa = 2.67
+u_gay = False
+
+print (type(age)) #this will print the data type of age variable which is integer
+
+age = float(age)
+print (age) # it will print 22.0 , means it convert int to float
