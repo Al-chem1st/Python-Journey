18a75fdc627030744bbf335271b50cfba098cd08
Em0n <cmrakibul.official@gmail.com>
2026-09-05 22:44:19 +0600

    part 1

---
+++ Day 2/1.arithmetic_and_math.py
@@ -0,0 +1,21 @@
+# learn about + , - , * , % , **
+friend = 10
+
+# friend = friend + 3 
+# friend += 3       this (+=) means friend = friend + 3
+
+# friend = friend -1
+# friend -= 1
+
+# friend = friend * 2
+# friend *=2
+
+# friend = friend / 2
+# friend /= 2
+
+# friend = friend **2      (** means friend = friend * friend , like squar)
+# friend **= 2
+
+#friend = friend % 3       (its quite deffent its means the remainder when divided by 3)    # friend = friend % 3 (friend = friend mod 3)  (mod means remainder)
+friend %= 3
+print(friend)
