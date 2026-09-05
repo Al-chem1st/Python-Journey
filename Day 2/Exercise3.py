3db05495f0afa4531473f22b1beb1a72a34e5d09
Em0n <cmrakibul.official@gmail.com>
2026-09-06 00:04:23 +0600

    If & Else Statements

---
+++ Day 2/Exercise3.py
@@ -0,0 +1,7 @@
+#Exercise 3 for If else statements
+
+propose = input("Do you love me? (yes/no): ")
+if propose == "yes":
+    print("I love you too!")    
+else:
+    print("That's okay, I understand that because I'm ugly.")
